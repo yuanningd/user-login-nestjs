dc953dac6cc8431d529ebed024a3acff242b30bc
yuanningd <65219674+yuanningd@users.noreply.github.com>
2023-09-01 18:15:14 +1000

    Update user.schema.ts

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
 export class User extends Document {
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   username: string;
 
   @Prop({ required: true })
